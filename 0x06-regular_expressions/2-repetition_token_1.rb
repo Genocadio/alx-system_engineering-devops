2e5be827e44b88b73b73d5d7d33bc938b6387af0
Geno <yvesgeno@outlook.com>
2023-08-01 22:08:14 +0200

    task 2

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,2 +1,13 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/^hb{0,1}tn$/).join
+if ARGV.length == 1
+  input_text = ARGV[0]
+  regex = /\bSchool\b/i
+  matches = input_text.scan(regex)
+  if matches.empty?
+    puts "No occurrences of 'School' found."
+  else
+    puts "Occurrences of 'School' found: #{matches.join(', ')}"
+  end
+else
+  puts "Please provide one argument with the input text."
+end
