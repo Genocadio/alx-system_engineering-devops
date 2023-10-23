3b6b72a3c8a75c79867add400ba0b797e595e4a6
geno <yvesgeno@outlook.com>
2023-10-23 22:44:13 +0200

    task 0 correction

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,14 +8,12 @@ if __name__ == "__main__":
     user_id = argv[1]
     user = requests.get(url + "users/{}".format(user_id)).json()
     todo = requests.get(url + "todos?userId={}".format(user_id)).json()
-    
+
     completed = [t for t in todo if t.get("completed")]
     num_completed_tasks = len(completed)
     total_tasks = len(todo)
 
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), num_completed_tasks, total_tasks))
-        
-    # Display completed task titles with proper formatting
     for task in completed:
         print("\t{}".format(task.get("title")))
