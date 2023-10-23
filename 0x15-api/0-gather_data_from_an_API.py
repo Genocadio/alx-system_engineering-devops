d9c0458bff42a310944a991511c5508912d71c5b
geno <yvesgeno@outlook.com>
2023-10-23 22:31:26 +0200

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,11 +8,8 @@ if __name__ == "__main__":
     user_id = argv[1]
     user = requests.get(url + "users/{}".format(user_id)).json()
     todo = requests.get(url + "todos?userId={}".format(user_id)).json()
-    completed = []
-    for task in todo:
-        if task.get("completed"):
-            completed.append(task)
+
+    completed = [t.get("title") for t in todo if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(todo)))
-    for task in completed:
-        print("\t {}".format(task.get("title")))
+    [print("\t {}".format(c)) for c in completed]
