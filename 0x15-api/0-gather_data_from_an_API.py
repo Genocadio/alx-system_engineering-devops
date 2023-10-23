970879cff0bdc221e7e41516f3c4981dd9aae2a8
geno <yvesgeno@outlook.com>
2023-10-23 22:42:29 +0200

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,43 +1,21 @@
 #!/usr/bin/python3
-"""A simple module to make API Calls
-To a mockup API server and return the
-Responses. Then print them out to standard output
-Usage: ./0-gather-data_from_an_API <ID>
-Where <ID> is the employee ID for whom we want to list
-The tasks"""
-
-from requests import get
+"""returns information about his/her TODO list progress."""
+import requests
 from sys import argv
 
-headers = {
-    "Content-Type": "application/json",
-    "Accept": "application/json",
-    "User-Agent": "Thing Gecko/20100101 Firefox/102.0"
-}
-base_url = "https://jsonplaceholder.typicode.com/users/"
-
-
-def get_task_status(user_id: str) -> None:
-    """
-    Get the task status for a certain user
-    Args:
-        user_id (str): The user id of the user
-    """
-    # lets first get the name of Employee
-    emp_name = get("{}{}".format(base_url, user_id)).json().get("name")
-    full_url = "{}{}/todos/".format(base_url, user_id)
-    response = get(full_url, headers=headers).json()
-    # lets get the total number of tasks shall we?
-    total_tasks = len(response)
-
-    # How about done tasks
-    done_tasks = [task['title'] for task in response
-                  if task['completed']]
-    done_tasks_count = len(done_tasks)
-    print("Employee {} is done with tasks({}/{}):".format(
-        emp_name, done_tasks_count, total_tasks))
-    [print("\t {}".format(task)) for task in done_tasks]
-
-
 if __name__ == "__main__":
-    get_task_status(argv[1])
+    url = "https://jsonplaceholder.typicode.com/"
+    user_id = argv[1]
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    todo = requests.get(url + "todos?userId={}".format(user_id)).json()
+    
+    completed = [t for t in todo if t.get("completed")]
+    num_completed_tasks = len(completed)
+    total_tasks = len(todo)
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), num_completed_tasks, total_tasks))
+        
+    # Display completed task titles with proper formatting
+    for task in completed:
+        print("\t{}".format(task.get("title")))
