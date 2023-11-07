ca9512d176872121aca7f2ef95775ff93e8a1643
geno <yvesgeno@outlook.com>
2023-11-07 22:23:36 +0200

    task 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Function that queries the Reddit API and returns the number of subscribers"""
+"""Function that queries the Reddit API and
+returns the number of subscribers"""
 
 
 def number_of_subscribers(subreddit):
