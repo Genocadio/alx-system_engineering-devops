c500769d4c41d4ab5c0cc3b6b9b38ec68d9d5e03
geno <yvesgeno@outlook.com>
2023-11-07 22:29:13 +0200

    task 1

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Function that queries the Reddit API and
+returns a list containing the titles of all hot articles"""
+import requests
+
+def recurse(subreddit, hot_list=[]):
+    """Function that queries the Reddit API and
+    returns a list containing the titles of all hot articles"""
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        for i in range(10):
+            hot_list.append(response.json().get('data').get('children')[i].get('data').get('title'))
+        return recurse(subreddit, hot_list)
+    else:
+        return hot_list
