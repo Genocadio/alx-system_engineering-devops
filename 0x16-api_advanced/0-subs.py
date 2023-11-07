12c0e483117371d485c7b33925c737e87a7cca62
geno <yvesgeno@outlook.com>
2023-11-07 22:18:07 +0200

    task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Function that queries the Reddit API and returns the number of subscribers"""
+
+
+def number_of_subscribers(subreddit):
+    """Function that queries the Reddit API and returns
+    the number of subscribers"""
+    import requests
+
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json().get('data').get('subscribers')
+    else:
+        return 0
