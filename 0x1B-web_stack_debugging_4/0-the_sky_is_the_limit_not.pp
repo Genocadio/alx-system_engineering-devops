a7774279ad748b9a5f92aa5942f000706cdc3906
geno <yvesgeno@outlook.com>
2023-11-16 19:04:06 +0200

    task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# replaces the first occurrence of "15" with "4096
+
+# Increase the ULIMIT
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+# nginx restart
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
